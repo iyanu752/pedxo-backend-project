f77a458ccb2196ce7894924bc01e026abe92dbc9
Israel-Adekanmi <111026062+Israel-Adekanmi@users.noreply.github.com>
2025-09-05 11:41:13 +0100

    Fix fetch assigned (#32)
    * feat: switch image upload to Cloudinary
    
    * feat: Fix Sending Contract Mail
    
    * feat: Create Talent Details
    
    * feat: Fetching Assigned Talent
    
    * feat: Admin Sign Up and Login
    
    * Merge branch 'main' of https://github.com/Pedxo/pedxo-backend-project
    
    * Merge branch 'main' of https://github.com/Pedxo/pedxo-backend-project
    
    * feat: Get All Hire
    
    * feat: Resolve Admin Guard
    
    * feat: Fix Fetch Assigned

--- src/hire/hire.controller.ts
+++ src/hire/hire.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Query, Post, UseGuards } from '@nestjs/common';
 import { HireService } from './hire.service';
 import { CurrentUser } from 'src/common/decorator/current.logged.user';
 import { AuthGuard } from 'src/auth/customGuard/guard.custom';
@@ -6,28 +6,29 @@ import { HireDTO } from './dto/hire.talent.dto';
 import { User } from 'src/user/schema/user.schema';
 
 @Controller('hire')
-@UseGuards(AuthGuard)
 export class HireController {
   constructor(private hireService: HireService) {}
 
   @Post('hire-talent')
+  @UseGuards(AuthGuard)
   async talent(@Body() hireInput: HireDTO, @CurrentUser() user: User) {
     return await this.hireService.talent(hireInput, user);
   }
 
   @Get('assigned-by-user')
+  @UseGuards(AuthGuard)
   getByUser(@CurrentUser() user: User) {
     // console.log(user._id);
     return this.hireService.getAssignedTalentsByUser(user._id);
   }
 
-  @Get('assigned-by-hire/:hireId')
-  getAssignedTalentByHire(@Param('hireId') hireId: string) {
+  @Get('assigned-by-hire')
+  getAssignedTalentByHire(@Query('hireId') hireId: string) {
     return this.hireService.getAssignedTalentByHireId(hireId);
   }
 
-  @Get('assigned-by-contract/:contractId')
-  getAssignedTalentByContract(@Param('contractId') contractId: string) {
+  @Get('assigned-by-contract')
+  getAssignedTalentByContract(@Query('contractId') contractId: string) {
     return this.hireService.getAssignedTalentByContractId(contractId);
   }
 
